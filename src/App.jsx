5b17fd9c826fc9a4fea84aaf5be40b751dbee9be
torshow <slywoods12@gmail.com>
2026-08-02 23:32:57 +0300

    add css styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,6 @@ import DispenserList from './components/DispenserList';
 import SearchBar from './components/SearchBar';
 import {modernDispensers} from './data';
 import { useState } from 'react';
-import './index.css'
-import './App.css'
-
 
 function App () {
   const [dispensers, setDispensers] = useState(modernDispensers);
@@ -21,6 +18,7 @@ function App () {
     ])
   }
 
+  
   //add search fuctionality
   const filteredDispensers = dispensers.filter((dispenser) => {
     const searchableText = `${dispenser.name} ${dispenser.type}`.toLowerCase();
@@ -29,22 +27,26 @@ function App () {
 
   return (
     <>
-    <Header/>
-    <main className='page-container'>
-      <div>
-        <p>Best Service Ever</p>
-        <h2>Get your best modern Dispenser through us </h2>
-        <p>
-          Connect with us Today for quality Dispensers
-        </p>
-      </div>
-    </main>
+          <Header/>
 
-    <DispenserForm addDispenser={addDispenser}/>
-    <SearchBar searchTerm={searchTerm} onSearch={setSearchTerm}/>
-    <DispenserList dispensers={filteredDispensers}/>
+          <main className='page-container'>
+              <div>
+                 <p>Best Service Ever</p>
+                 <h2>Get your best modern Dispenser through us </h2>
+                 <p>
+                    Connect with us Today for quality Dispensers
+                 </p>
+              </div>
+          </main>
+
+          <DispenserForm addDispenser={addDispenser} />
+          <SearchBar searchTerm={searchTerm} onSearch={setSearchTerm} />
+          <DispenserList dispensers={filteredDispensers} />
+
     </>
+
   )
+  
 }
 
 export default App;
